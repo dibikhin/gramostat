9e71b1056a528d8e8ba31e6c8729047f1892c8a3
Roman Dibikhin <dibikhin@gmail.com>
2017-10-21 15:49:44 +0300

    Update gramostat.js

--- gramostat.js
+++ gramostat.js
@@ -1,6 +1,5 @@
 /// Gramostat
 ///
-/// TODO Use "use strict", Object.freeze, lambdas, Pure functions, generators&iterators, modules
 ///
 /// Sample usage:
 // var import_script = (function (oHead) {
@@ -53,12 +52,11 @@
 
 	})(document.head || document.getElementsByTagName("head")[0]);
 
-	// TODO: compare by id
 	function stats(yesterday_followers, today_followers) {
 		console.log('Computing stats...');
 
-		var today_followers_names = _.map(today_followers, function (item) { return item.node.username; });
-		var yesterday_followers_names = _.map(yesterday_followers, function (item) { return item.node.username; });
+		var today_followers_names = _followers_to_usernames(today_followers);
+		var yesterday_followers_names = _followers_to_usernames(yesterday_followers);
 
 		var intersection = _.intersection(yesterday_followers_names, today_followers_names);
 		var unsubscribed = _.difference(yesterday_followers_names, intersection);
@@ -77,22 +75,41 @@
 		};
 	}
 
-	// Gramostat.fetch_common_followers(323124269, 510162439) // 323124269 - Тома , 510162439 - Алена)
-	/// returns [{}]
-	function fetch_common_followers(id_one, id_two) {
-		var id_one_followers = export_followers(id_one);
-		var id_two_followers = export_followers(id_two);
+	/**
+	 * Converts followers to usernames
+	 * 
+	 * @param {object[]} followers Followers
+	 * @returns {string[]} Usernames of followers
+	 */
+	function _followers_to_usernames(followers) {
+		return _.map(followers, item => item.node.username);
+	}
 
-		var id_one_followers_names = _.map(id_one_followers, function (item) { return item.node.username; });
-		var id_two_followers_names = _.map(id_two_followers, function (item) { return item.node.username; });
+	/**
+	 * Finds common followers' usernames
+	 * 
+	 * @param {object[]} followers Followers list one
+	 * @param {object[]} other_followers Followers list two
+	 * @returns {string[]} Usernames of followers
+	 */
+	function find_common_followers(followers, other_followers) {
+		return _.intersection(
+			_followers_to_usernames(followers),
+			_followers_to_usernames(other_followers));
+	}
 
-		return _.intersection(id_one_followers_names, id_two_followers_names);
-
-		// fetch_common_followers(export_followers(id_one), export_followers(id_two))
-		// function to_usernames(items) { return _.map(items, item = > return item.node.username); }
-		// return _.intersection(
-		//		to_names(followers_one),
-		// 		to_names(followers_two));
+	/**
+	 * Fetches followers and find common ones
+	 * Gramostat.fetch_common_followers(323124269, 510162439) // 323124269 - Тома , 510162439 - Алена)
+	 * 
+	 * @param {number} user_id User Id
+	 * @param {number} other_user_id User Id
+	 * @returns {string[]} Usernames of followers
+	 */
+	function fetch_common_followers(user_id, other_user_id) {
+		return find_common_followers(
+			export_followers(user_id),
+			export_followers(other_user_id));
 	}
 
 	// Gramostat.export_followers(323124269)
@@ -128,8 +145,138 @@
 		return followers;
 	}
 
-	// https://www.instagram.com/dibikhin/?__a=1
+	/**
+	 * Gets user by username
+	 * 
+	 * Uses https://www.instagram.com/dibikhin/?__a=1
+	 * 
+	 * @param {String} username 
+	 * @returns {Object} User
+	 */
+	function get_user_by_username(username) {
+		return _getSync('https://www.instagram.com/' + username + '/?__a=1');
+	}
 
+	/**
+	 * Compute community reachability by followings qty
+	 * 
+	 * Category range: from Unreachable (2K+) to Very reachable (<100)
+	 * 
+	 * @param {object} user User 
+	 * @returns {string} Category
+	 */
+	function reachability(user) {
+		return 'nope';
+	}
+
+	/**
+	 * ***NOT PURE***
+	 * 
+	 * Fetches users and computes grouped stats of influence
+	 * 
+	 * influence_stats(dri_fols_stats_201017.subscribed);
+	 * 
+	 * @param {string[]} usernames Usernames
+	 * @returns {object[]} Grouped by category users' stats
+	 */
+	function influence_stats(usernames) {
+		var stats = _.map(usernames, function (username) {
+			return {
+				username: username,
+				influence: _influence(get_user_by_username(username))
+			};
+		});
+
+		return _.groupBy(stats, user_stats => user_stats.influence);
+	}
+
+	/**
+	 * ***NOT PURE***
+	 * 
+	 * Fetches users and computes grouped stats of activity
+	 * 
+	 * activity_stats(dri_fols_stats_201017.subscribed);
+	 * 
+	 * @param {string[]} usernames Usernames
+	 * @returns {object[]} Grouped by category users' stats
+	 */
+	function activity_stats(usernames) {
+		var stats = _.map(usernames, function (username) {
+			return {
+				username: username,
+				activity: _activity(get_user_by_username(username))
+			};
+		});
+
+		return _.groupBy(stats, user_stats => user_stats.activity);
+	}
+
+	/**
+	 * Compute user's influence by followers to following ratio.
+	 * Absolute followers' qty ignored.
+	 * 
+	 * Category range: from Mass follower (<=0.5) to Influener (40+).
+	 * 
+	 * @param {object} user User
+	 * @returns {string} Category
+	 */
+	function _influence(user_info) {
+		if (!user_info || !user_info.user
+			|| user_info.user.followed_by.count < 1 || user_info.user.follows.count < 1) {
+			return 'N/A';
+		}
+
+		var user = user_info.user;
+
+		var followers_ratio = user.followed_by.count / user.follows.count;
+
+		if (followers_ratio <= 0.5) {
+			return 'Mass follower';
+		} else if (followers_ratio <= 1) {
+			return 'Low popularity';
+		} else if (followers_ratio <= 2) {
+			return 'Regular';
+		} else if (followers_ratio <= 5) {
+			return 'Popular';
+		} else if (followers_ratio < 40) {
+			return 'Rising star';
+		} else {
+			return 'Influencer';
+		}
+	}
+
+	/**
+	* Compute absolute user's activity by total media qty.
+	* Last activity doesn't matter.
+	* 
+	* Category range: from Inactive (0) to Power users (1000+).
+	* 
+	* @param {object} user User
+	* @returns {string} Category
+	*/
+	function _activity(user_info) {
+		if (!user_info || !user_info.user || !user_info.user.media || user_info.user.media.count < 0) {
+			return 'N/A';
+		}
+
+		var media_count = user_info.user.media.count;
+
+		if (media_count === 0) {
+			return 'Inactive';
+		} else if (media_count < 50) {
+			return 'Low activity';
+		} else if (media_count < 100) {
+			return 'Active';
+		} else if (media_count < 500) {
+			return 'Very active';
+		} else if (media_count < 1000) {
+			return 'Power user';
+		} else {
+			return 'Maniac';
+		}
+	}
+
+	// https://www.instagram.com/knl_100/?__a=1 -> 404
 	function _getSync(url) {
 		var result = null;
 		jQuery.ajax({
@@ -152,9 +299,23 @@
 
 	Gramostat = {
 		fetch_common_followers: fetch_common_followers,
+		find_common_followers: find_common_followers,
 		export_followers: export_followers,
 		stats: stats,
+		get_user_by_username: get_user_by_username,
+		influence_stats: influence_stats,
+		activity_stats: activity_stats,
 		test: test
 	};
 	console.log('Loaded!');
 })();
+
+// TODO combined activity and influence stats
+// TODO assign Gramostat in place
+// TODO Use "use strict", Object.freeze, lambdas, Pure functions, generators&iterators, modules
+// TODO stats(): compare objects w/ mixin Underscore's createIndexFinder with _.isEqual
+// TODO extract_migrated_followers()
+// TODO add _.throttle by hours for care with Instagram limits
+// TODO everyday run: export, stats
+// TODO add _.memoize and cache reloading
+// TODO store history to Firebase
